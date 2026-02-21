e743f2b371827c21ada3861d9d46a998d15e0ed8
Shann-Salamingan1048 <salaminganshann@gmail.com>
2026-02-21 15:08:27 +0800

    Added Linux Enabler GPU

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 #include "GettingStarted.h"
 
+#ifdef _WIN32
+#include <windows.h>
 extern "C"
 {
+    // These tell the Windows drivers to use the dedicated GPU by default
     __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
     __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
+#endif
+#include <cstdlib>
+#include <iostream>
+
+#ifdef __linux__
+void ensure_high_performance_gpu() {
+    if (std::getenv("__NV_PRIME_RENDER_OFFLOAD") == nullptr) {
+        std::cout << "Restarting with NVIDIA GPU..." << std::endl;
+        setenv("__NV_PRIME_RENDER_OFFLOAD", "1", 1);
+        setenv("__GLX_VENDOR_LIBRARY_NAME", "nvidia", 1);
+        // This part is messy: you have to re-execute your own binary
+    }
+}
+#endif
 int main()
 {
+    ensure_high_performance_gpu();
     GettingStarted::GetStart gs(1200,1200, "Getting Started");
     //gs.HelloTriangle();
     //gs.HelloSquare();
--- stb.cpp
+++ stb.cpp
@@ -1,2 +1,2 @@
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb\stb_images.h>
+#include <stb/stb_images.h>
